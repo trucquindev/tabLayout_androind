472b64459901818d98df67cedc414137fda5e9f0
quynhdeptrai306 <nguyentrucquynh3006@gmail.com>
2024-10-13 17:23:37 +0700

    custom tab layout

---
+++ app/src/main/java/com/example/tablayout/order/OrderViewPagerAdapter.java
@@ -0,0 +1,52 @@
+package com.example.tablayout.order;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import com.example.tablayout.fragment.AccountFragment;
+import com.example.tablayout.fragment.BlankFragment;
+import com.example.tablayout.fragment.OderFragment;
+
+public class OrderViewPagerAdapter extends FragmentStatePagerAdapter {
+    public OrderViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new Tab1Fragment();
+            case 1:
+                return new Tab2Fragment();
+            case 2:
+                return new Tab3Fragment();
+            default:
+                return new Tab1Fragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "Tab 1";
+            case 1:
+                return "Tab 2";
+            case 2:
+                return "Tab 3";
+            default:
+                return "Tab 1";
+        }
+    }
+}
